ae6d9767ab5c7f932d4f898d6953f7a9f85b7896
sam bacha <sam@freighttrust.com>
2021-10-19 10:03:42 -0700

    fix(sh): base

--- base.sh
+++ base.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 # Linux Paranoia - Security Settings 
-// randomizes page allocator freelists,
+#// randomizes page allocator freelists,
 page_alloc.shuffle=1
 
-//  enables Kernel Page Table Isolation which mitigates Meltdown and prevents some KASLR bypasses. 
+#//  enables Kernel Page Table Isolation which mitigates Meltdown and prevents some KASLR bypasses. 
 pti=on
 
-// disables vsyscalls as they are obsolete and have been replaced with vDSO
+#// disables vsyscalls as they are obsolete and have been replaced with vDSO
 vsyscall=none
 
-// only signed kernal modules
+#// only signed kernal modules
 module.sig_enforce=1
 
-// LSM mode
+#// LSM mode
 lockdown=confidentiality
 
-// disable webcam
+#// disable webcam
 install uvcvideo /bin/false
 
-// debian dont be retarded
-// see: https://manpages.debian.org/buster/dpkg/dpkg-statoverride.1.en.html
+#// debian dont be retarded
+#// see: https://manpages.debian.org/buster/dpkg/dpkg-statoverride.1.en.html
 chmod 700 /home/$user
 chmod 700 /boot /usr/src /lib/modules /usr/lib/modules
